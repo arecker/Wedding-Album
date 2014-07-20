9c01e6d751bfb68bfc5e89ab7e62055b36288f69
Alex Recker <alex@reckerfamily.com>
2014-07-20 11:01:57 -0500

    zipping and indexes

--- src/script.py
+++ src/script.py
@@ -1,21 +1,48 @@
-from os import listdir
+from os import listdir, walk
+import zipfile
 from os.path import abspath, dirname, join
 from jinja2 import Environment, FileSystemLoader
+
+class Image:
+	def __init__(self, src, number):
+		self.src = src
+		self.number = number
 
 def generate_page():
 	src_path = dirname(abspath(__file__))
 	public_path = abspath(join(src_path, '..', 'public'))
 	images_path = abspath(join(public_path, 'images'))
+	zip_path = abspath(join(public_path, 'ReckerWeddingPictures.zip'))
 
 	images = []
+	i = 1
 	for file in listdir(images_path):
-		images.append("images/" + file)
+		if file != '.gitignore':
+			images.append(Image("images/" + file, i))
+			i = i + 1
 
 	env = Environment(loader=FileSystemLoader(src_path))
 	template = env.get_template('template.html')
 
 	with open(join(public_path, 'index.html'), "wb") as file:
 		file.write(template.render(images=images))
+
+	zipper(images_path, zip_path)
+
+
+def zipper(dir, zip_file):
+    zip = zipfile.ZipFile(zip_file, 'w', compression=zipfile.ZIP_DEFLATED)
+    root_len = len(abspath(dir))
+    for root, dirs, files in walk(dir):
+        archive_root = abspath(root)[root_len:]
+        for f in files:
+        	if f != '.gitignore':
+	            fullpath = join(root, f)
+	            archive_name = join(archive_root, f)
+	            print f
+	            zip.write(fullpath, archive_name, zipfile.ZIP_DEFLATED)
+    zip.close()
+    return zip_file
 
 if __name__ == '__main__':
 	generate_page()
